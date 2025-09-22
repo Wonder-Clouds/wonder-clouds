c22dfa565ab0bae473f07e5fca88e3084c7efcdf
Cristian Monzon Guzman <cristian-02-@live.com>
2025-09-22 10:40:52 -0500

    Modificando el script de migraciones

--- backend/wait_for_db.sh
+++ backend/wait_for_db.sh
@@ -10,7 +10,7 @@ done
 echo "Base de datos lista. Ejecutando migraciones..."
 
 # Ejecutar makemigrations solo para las aplicaciones específicas que se indican
-python manage.py makemigrations blog category client smtp tag -noinput
+python manage.py makemigrations -noinput
 
 # Ejecutar migrate para aplicar las migraciones pendientes de las aplicaciones especificadas
 python manage.py migrate --noinput

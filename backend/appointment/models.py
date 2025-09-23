393550b8d1a8a74a0cc003307bdd5f0b63e30a5d
McGlovin <018200684B@uandina.edu.pe>
2025-09-23 00:42:01 -0500

    Modulo de appointment

--- backend/appointment/models.py
+++ backend/appointment/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel, SOFT_DELETE
+import uuid
+
+from client.models import Client
 
 # Create your models here.
+class Appointment(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE
+
+    STATUS_CHOICES = [('PN', 'pending'), ('CF','confirmed'), ('FN','finish'), ('NT', 'Nothing')]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField()
+    time = models.TimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(choices=STATUS_CHOICES)
+    subject = models.CharField(max_length=100, editable=False)
+    detail = models.TextField()
+
+    # Relations
+    client = models.ForeignKey(Client, related_name='client', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.status} {self.subject}'
+    
